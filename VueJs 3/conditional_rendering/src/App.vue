620c0818e6650761e3488f69e481899843fa92e2
aashaykadu <aashayk@cybage.com>
2024-04-15 18:39:50 +0530

    3 topics

---
+++ VueJs 3/conditional_rendering/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2 v-if="num === 0">The number is zero</h2>
+    <h2 v-else-if="num < 0">The number is negative</h2>
+    <h2 v-else-if="num > 0">The number is positive</h2>
+    <h2 v-else>Not a number</h2>
+    <template v-if="display">
+      <h2>Aashay Kadu</h2>
+      <h2>Codevolution</h2>
+      <h2>Vue</h2>
+    </template>
+    <h2 v-show="showElement">Using v-show</h2>
+    <!-- v-show will be always rendered to the dom -->
+    <h2 v-if="showElement">Using v-if</h2>
+    <!-- v-if will only rendered to the dom if condition is satisfied -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      num: "0",
+      display: true,
+      showElement:true,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueJs 3/listRendering/list-rendering/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+    <h2 v-for="name in fullNames" :key="name.first">
+      {{ name.first }} {{ name.last }}
+    </h2>
+    <div v-for="actor in actors" :key="actor.name">
+      <h2>{{ actor.name }}</h2>
+      <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
+    </div>
+    <h2 v-for="(value, key, index) in myInfo" :key="value">
+      {{ index }} {{ key }} {{ value }}
+    </h2>
+    <template v-for="i in names" :key="i">
+      <h2>{{ i }}</h2>
+      <hr />
+    </template>
+
+    <template v-for="name in names" :key="name">
+      <h2 v-if="name === 'Bruce'">{{ name }}</h2></template
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      names: ["Bruce", "Clark", "Diana"],
+      fullNames: [
+        { first: "Bruce", last: "Wayne" },
+        { first: "Clark", last: "Kent" },
+        { first: "Princess", last: "Diana" },
+      ],
+      actors: [
+        {
+          name: "Christian Bale",
+          movies: ["Batman", "The Prestige"],
+        },
+        {
+          name: "Di Caprio",
+          movies: ["Titanic", "Inception"],
+        },
+      ],
+      myInfo: {
+        name: "Ashley",
+        channel: "carvaka podcast",
+        course: "vue 3",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
